8a7ee2697018e0c4acd4c1ae0643cb15576ac3ca
ben <ben@bfox.dev>
2023-03-08 16:35:04 -0500

    refactored widths

--- Components/Footer.tsx
+++ Components/Footer.tsx
@@ -3,8 +3,10 @@ import styles from "../styles/Footer.module.css";
 export default function Footer() {
   return (
     //make a footer with a name, email, and a tagline
-    <div className={styles.footerLogo}>
-      <p className={styles.footerName}>🤍 Ben Fox 🤍</p>
-    </div>
+    <footer className={styles.footer__container}>
+      <div className={styles.footerBranding}>
+        <p className={styles.footerName}>🤍 Ben Fox 🤍</p>
+      </div>
+    </footer>
   );
 }
