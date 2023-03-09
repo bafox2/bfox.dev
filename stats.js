d84becbe39a285a483935d954bbc4373f8de1982
ben <ben@bfox.dev>
2023-03-08 21:38:30 -0500

    stats grid added

---
+++ stats.js
@@ -0,0 +1,68 @@
+export default stats = [
+  {
+    heading: 'Syntax Episodes Listened To',
+    data: 100,
+    description: 'Scott and Wes, thanks for helping me get excited about becoming a developer',
+    joke: false
+  },
+  {
+    heading: 'GitHub Repositories',
+    data: 10,
+    description: 'I have a lot of work to do',
+    joke: false
+  },
+  {
+    heading: 'Days Pushing A Commit',
+    data: 168,
+    description: 'I have a lot of work to do',
+    joke: false
+  },
+  {
+    heading: 'Lines of Code Written',
+    data: 100000,
+    description: 'I have a lot of work to do',
+    joke: false
+  },
+  {
+    heading: 'Obsidian Notes in Programming Folder',
+    data: 100,
+    description: 'I have a lot of work to do',
+    joke: false
+  },
+  {
+    heading: 'YouTube Videos Watched', 
+    data: 100,
+    description: 'I have a lot of work to do',
+    joke: false
+  },
+  {
+    heading: 'Git Reset --hard Count',
+    data: 100,
+    description: 'I have a lot of work to do',
+    joke: true,
+  },
+  {
+    heading: 'JS Frameworks Considered',
+    data: 100,
+    description: 'I have a lot of work to do',
+    joke: true,
+  },
+  {
+    heading: 'Ideas Written Past Midnight',
+    data: 100,
+    description: 'I have a lot of work to do',
+    joke: true,
+  },
+  {
+    heading: 'CSS Tricks Articles Bookmarked',
+    data: 100,
+    description: 'I have a lot of work to do',
+    joke: true,
+  },
+  {
+    heading: 'Stack Overflow In Search History',
+    data: 100,
+    description: 'I have a lot of work to do',
+    joke: true,
+  },
+];
